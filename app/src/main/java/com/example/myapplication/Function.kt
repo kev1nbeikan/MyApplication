38164bfda1be367ea84b037f9e7cd2292a3bea5c
turnipsh <t>
2023-09-02 15:11:26 +0300

    add recycleView

---
+++ app/src/main/java/com/example/myapplication/Function.kt
@@ -0,0 +1,62 @@
+package com.example.myapplication
+
+import android.app.Application
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.databinding.FanctionItemBinding
+
+data class Function(val imageId: Int, val nameId: Int)
+
+class FunctionAdapter : RecyclerView.Adapter<FunctionAdapter.FunctionVeiwHolder>() {
+    var data: List<Function> = emptyList()
+        set(newValue) {
+            field = newValue
+            notifyDataSetChanged()
+        }
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FunctionVeiwHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = FanctionItemBinding.inflate(inflater, parent, false)
+
+        return FunctionVeiwHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: FunctionVeiwHolder, position: Int) {
+        val function = data[position]
+
+        with(holder.binding) {
+            imageOfFunctionViewInRecycleView.setImageResource(function.imageId)
+            nameOfFunctionViewInRecycleView.setText(function.nameId)
+        }
+    }
+
+    class FunctionVeiwHolder(val binding: FanctionItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
+}
+
+
+class App : Application() {
+    val functionService = FunctionService()
+}
+
+class FunctionService {
+
+    private var functions = mutableListOf<Function>(
+        Function(
+            imageId = R.mipmap.flashlight,
+            nameId = R.string.flashLight,
+            ),
+        Function(
+            imageId = R.mipmap.bluetooth,
+            nameId = R.string.bluetooth,
+        )
+    )
+
+    fun getFunctions(): MutableList<Function> {
+        return functions
+    }
+
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +14,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var turnFunctionButton: Button
     private lateinit var imageOfFunctionView: ImageView;
     private lateinit var nameOFFunctionView: TextView;
+
+    private lateinit var adapter: FunctionAdapter
+//    private val functionService: FunctionService
+//        get() = (applicationContext as App).functionService
 
     lateinit var flashLight: FlashLight;
 
@@ -24,10 +28,21 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        bindFunctionAdapter()
+
         getFlashLight();
 
         setDefaultFunction();
 
+    }
+
+    private fun bindFunctionAdapter() {
+        val manager = LinearLayoutManager(this)
+        adapter = FunctionAdapter()
+        adapter.data = FunctionService().getFunctions()
+
+        binding.functionsRecycleView.adapter = adapter
+        binding.functionsRecycleView.layoutManager = manager
     }
 
     private fun getFlashLight() {
@@ -54,13 +69,22 @@ class MainActivity : AppCompatActivity() {
             R.string.flashLight -> button.setOnClickListener {
                 if (flashLight.getTurnStatus()) {
                     flashLight.turnOff()
+                    changeTurnButtonLabelToOn()
                 } else {
                     flashLight.turnOn();
+                    changeTurnButtonLabelToOff()
                 }
             }
         }
     }
 
+    private fun changeTurnButtonLabelToOn() {
+        turnFunctionButton.setText(R.string.turnOn)
+    }
+
+    private fun changeTurnButtonLabelToOff() {
+        turnFunctionButton.setText(R.string.turnOff)
+    }
 
 
     private fun bindAll() {
